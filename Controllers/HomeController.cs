aa139e44915202fe12eb4646d15dd54c20aeab76
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-07-09 14:51:35 +0100

    Valid solution.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using WeatherApp.Models;
+using WeatherApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<CityWeather> cityWeatherList = DataService.GetCityWeatherList();
+            ViewData["Title"] = "Weather";
+            return View(cityWeatherList);
+        }
+
+        [Route("weather/{cityCode}")]
+        public IActionResult Weather(string cityCode)
+        {
+            CityWeather? model = DataService.GetCityWeatherList()
+                .Where(c => c.CityUniqueCode == cityCode.ToUpper())
+                .FirstOrDefault();
+
+            if (model != null)
+            {
+                ViewData["Title"] = "Weather";
+                return View(model);
+            }
+            else
+            {
+                ViewData["Title"] = "Error 404:";
+                ViewData["Error"] = $"There is no city with Unique City Code: {cityCode.ToUpper()}";
+				return View("Error");
+			}
+        }
+    }
+}
---
+++ Services/DataService.cs
@@ -0,0 +1,33 @@
+﻿using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+	public class DataService
+	{
+		// Service purely to return hard-coded data as per brief.
+		private static readonly List<CityWeather> _cityWeatherList = new List<CityWeather>()
+		{
+			new CityWeather() {
+				CityUniqueCode = "LDN",
+				CityName = "London",
+				DateAndTime = DateTime.Parse("2030-01-01 8:00"),
+				TemperatureFahrenheit = 33
+			},
+			new CityWeather() {
+				CityUniqueCode = "NYC",
+				CityName = "New York",
+				DateAndTime = DateTime.Parse("2030-01-01 3:00"),
+				TemperatureFahrenheit = 60
+			},
+			new CityWeather() {
+				CityUniqueCode = "PAR",
+				CityName = "Paris",
+				DateAndTime = DateTime.Parse("2030-01-01 9:00"),
+				TemperatureFahrenheit = 82
+			}
+		};
+
+		public static List<CityWeather> GetCityWeatherList() => _cityWeatherList;
+		
+	}
+}
